2292810ba80d8e55b3ca3b6c0642bd31e8647a21
Eklak Dangaura <azens1995@gmail.com>
2020-05-16 11:21:39 +0545

    NR: initial commit

---
+++ app/src/main/java/com/azens1995/notes_room/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.azens1995.notes_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
